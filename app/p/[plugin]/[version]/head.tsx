9e50d0f13bc6936fb3fd92680309cc8a9baddcfb
brokiem <milyhqvideo@gmail.com>
2023-02-24 16:21:53 +0800

    Add meta tags

--- app/p/[plugin]/[version]/head.tsx
+++ app/p/[plugin]/[version]/head.tsx
@@ -16,6 +16,23 @@ export default async function Head({ params, }: { params: { plugin: string, vers
   const plugin = await getPlugin(params.plugin, params.version);
 
   return (
-    <title>{plugin?.name + " v" + plugin?.version + " by " + plugin?.repo_name.split("/")[0] + " - BetterPoggit"}</title>
+    <>
+      <title>{plugin?.name + " v" + plugin?.version + " by " + plugin?.repo_name.split("/")[0] + " - BetterPoggit"}</title>
+
+      <meta name="title" content={plugin?.name + " v" + plugin?.version + " by " + plugin?.repo_name.split("/")[0] + " - BetterPoggit"} />
+      <meta name="description" content={plugin?.tagline} />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://better-poggit.hop.sh/" />
+      <meta property="og:title" content={plugin?.name + " v" + plugin?.version + " by " + plugin?.repo_name.split("/")[0] + " - BetterPoggit"} />
+      <meta property="og:description" content={plugin?.tagline} />
+      <meta property="og:image" content={plugin?.icon_url} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://better-poggit.hop.sh/" />
+      <meta property="twitter:title" content={plugin?.name + " v" + plugin?.version + " by " + plugin?.repo_name.split("/")[0] + " - BetterPoggit"} />
+      <meta property="twitter:description" content={plugin?.tagline} />
+      <meta property="twitter:image" content={plugin?.icon_url} />
+    </>
   );
 }
