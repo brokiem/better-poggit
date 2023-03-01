d20f1118f90daa57584cec590a6bfc846c3e1de5
brokiem <milyhqvideo@gmail.com>
2023-03-02 07:16:53 +0800

    Fix next js <ul> <li> key warning

--- components/pagination/Pagination.tsx
+++ components/pagination/Pagination.tsx
@@ -31,13 +31,13 @@ export default function Pagination({ plugins, currentPage }: { plugins: Plugin[]
 
     if (i === currentPageNum) {
       pageButtons.push(
-        <a href={"/plugins/page/" + i} className="flex items-center z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">
+        <a key={i} href={"/plugins/page/" + i} className="flex items-center z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">
           {i}
         </a>
       );
     } else {
       pageButtons.push(
-        <li>
+        <li key={i}>
           <a href={"/plugins/page/" + i} className="flex items-center px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
             {i}
           </a>
@@ -48,7 +48,7 @@ export default function Pagination({ plugins, currentPage }: { plugins: Plugin[]
 
   if (!isMaxPageButtonShown) {
     pageButtons.push(
-      <li>
+      <li key="...">
         <p
           className="flex items-center px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
           ...
@@ -57,7 +57,7 @@ export default function Pagination({ plugins, currentPage }: { plugins: Plugin[]
     );
 
     pageButtons.push(
-      <li>
+      <li key={totalPages}>
         <a href={"/plugins/page/" + totalPages}
            className="flex items-center px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
           {totalPages}
@@ -70,14 +70,14 @@ export default function Pagination({ plugins, currentPage }: { plugins: Plugin[]
     <>
       <nav aria-label="Page navigation example" className="flex items-center justify-center mt-10">
         <ul className="inline-flex items-center -space-x-px">
-          <li>
+          <li key="previous">
             <a href={"/plugins/page/" + (currentPageNum <= 1 ? 1 : currentPageNum - 1)} className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
               <span className="sr-only">Previous</span>
               <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" /></svg>
             </a>
           </li>
           {pageButtons}
-          <li>
+          <li key="next">
             <a href={"/plugins/page/" + (currentPageNum >= totalPages ? totalPages : currentPageNum + 1)} className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
               <span className="sr-only">Next</span>
               <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg>
